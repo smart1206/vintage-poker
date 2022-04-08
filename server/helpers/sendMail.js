0c3192f991bccfd0befd612eb82cf9145f033e7a
smart1206 <proharvester@outlook.com>
2022-04-08 20:10:07 +0800

    ignore some

--- server/helpers/sendMail.js
+++ server/helpers/sendMail.js
@@ -4,24 +4,24 @@ const config = require('../config');
 const sendEmail = async (sendToEmail, template) => {
   const { subject, text, html } = template;
 
-  const transporter = nodemailer.createTransport({
-    host: config.SMTP_HOST,
-    port: config.SMTP_PORT,
-    auth: {
-      user: config.SMTP_USER,
-      pass: config.SMTP_PW,
-    },
-  });
+  // const transporter = nodemailer.createTransport({
+  //   host: config.SMTP_HOST,
+  //   port: config.SMTP_PORT,
+  //   auth: {
+  //     user: config.SMTP_USER,
+  //     pass: config.SMTP_PW,
+  //   },
+  // });
 
-  const message = {
-    from: `${config.FROM_NAME} <${config.FROM_EMAIL}>`,
-    to: sendToEmail,
-    subject,
-    text,
-    html,
-  };
+  // const message = {
+  //   from: `${config.FROM_NAME} <${config.FROM_EMAIL}>`,
+  //   to: sendToEmail,
+  //   subject,
+  //   text,
+  //   html,
+  // };
 
-  const info = await transporter.sendMail(message);
+  // const info = await transporter.sendMail(message);
 
   console.log('Message sent: %s', info.messageId);
 };
